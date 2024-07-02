1bb3a200ef879682255caa906194c5ab06e50aee
Juan Coronado <coronajh19@gmail.com>
2024-07-02 15:59:33 +0200

    Endpoint para crear una nota

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,14 +4,6 @@ import listTaskByIdcontroller from "./listTaskById.controller.js";
 import createNoteController from "./createNote.controller.js";
 
 export {
-  createTaskController,
-  listTaskController,
-  listTaskByIdcontroller,
-  createNoteController,
+  createTaskController, listTaskController, listTaskByIdcontroller, createNoteController,
 };
 
-// import createTaskController from "./createTask.controller.js";
-// import listTaskController from "./listTask.controller.js";
-// import listTaskByIdcontroller from "./listTaskById.controller.js";
-// import createNoteController from "./createNote.controller.js";
-// export { createTaskController, listTaskController, listTaskByIdcontroller, createNoteController };
