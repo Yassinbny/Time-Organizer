417eab5e3f54e58187459bc498ce1026bbf52e25
Robert Muresan <robotzelu89@gmail.com>
2024-07-04 15:40:19 +0100

    add column enabled to user

--- src/db/createTables.js
+++ src/db/createTables.js
@@ -10,6 +10,7 @@ const createTables = async () => {
         email VARCHAR(255) UNIQUE NOT NULL,
         password VARCHAR(255) NOT NULL,
         token CHAR(30) NULL,
+        enabled BOOLEAN NOT NULL DEFAULT TRUE,
         role ENUM('admin', 'normal') DEFAULT 'normal',
         createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
         updatedAt DATETIME ON UPDATE CURRENT_TIMESTAMP,
