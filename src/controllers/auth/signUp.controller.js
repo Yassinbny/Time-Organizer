ca4a4a72042a9128a236f1ffe6d1d7cd4a242c48
Juan Coronado <coronajh19@gmail.com>
2024-07-31 16:09:20 +0200

    ruta m

--- src/controllers/auth/signUp.controller.js
+++ src/controllers/auth/signUp.controller.js
@@ -3,47 +3,38 @@ import generateToken from "../../services/generateToken.js";
 import sendMail from "../../services/sendMail.js";
 import validateSchema from "../../validations/validateSchema.js";
 import { signUpSchema } from "../../validations/usersSchema.js";
+import { FRONTEND_URL } from "../../env.js";  // Asegúrate de importar esta variable
 
 export default async function signUpController(req, res, next) {
   try {
     const { username, email, password } = req.body;
 
-    //Validar el body con Joi.
+    // Validar el cuerpo de la solicitud con Joi.
     await validateSchema(signUpSchema, req.body);
 
-    //Generar token aleatorio.
+    // Generar un token de confirmación único.
     const signUpCode = generateToken();
 
-    //Generar el correo.
-    const emailSubject = "Confirma tu registro en Time Organizer.";
+    // Crear el enlace de confirmación usando FRONTEND_URL.
+    const emailLink = `${FRONTEND_URL}/confirm/${signUpCode}`;
 
-    //Link para confirmar el registro.
-    const emailLink = `${req.protocol}://${req.get(
-      "host"
-    )}/users/confirm/${signUpCode}`;
-
-    //cuerpo del mail
+    // Crear el cuerpo del correo electrónico.
     const emailBody = `
-          <!DOCTYPE html>
-          <html lang="es">
-          <body>
-              <h2>¡Bienvenid@ ${username}!</h2>
-              <h3>Gracias por registrarte en Time Organizer.</h3>
-              <hr>
-              <p>Haz click en el siguiente <a href="${emailLink}">enlace</a> para completar tu registro.</p>
-          </body>
-          </html>
-        `;
+      <!DOCTYPE html>
+      <html lang="es">
+      <body>
+          <h2>¡Bienvenid@ ${username}!</h2>
+          <h3>Gracias por registrarte en Time Organizer.</h3>
+          <hr>
+          <p>Haz click en el siguiente <a href="${emailLink}">enlace</a> para completar tu registro.</p>
+      </body>
+      </html>
+    `;
 
-    //Pasar al modelo la consulta con la DB.
-    const { message } = await signUpModel(
-      username,
-      email,
-      password,
-      signUpCode
-    );
+    // Guardar el usuario en la base de datos con el código de confirmación.
+    const { message } = await signUpModel(username, email, password, signUpCode);
 
-    //Enviar el correo de registro.
+    // Enviar el correo electrónico con el enlace de confirmación.
     await sendMail(email, emailSubject, emailBody);
 
     return res.status(201).json({
