f2ed9cc3ebea9d78f05ca59ef11b2ae0abae9ed6
Inez Siepak <inez.siepak@gmail.com>
2024-08-08 21:09:56 +0200

    Actualización del correo de recuperación de la contraseña

--- src/controllers/users/recoverPass.controller.js
+++ src/controllers/users/recoverPass.controller.js
@@ -15,16 +15,20 @@ const recoverPassController = async (req, res, next) => {
     const emailBody = `
       <!DOCTYPE html>
           <html lang="es">
-          <body>
-              <h3>Se ha solicitado la recuperación de contraseña para este email en Time Organizer.</h3> 
-              <hr>
-              <h4>Utiliza el siguiente código para crear una nueva contraseña: ${recoverPassCode}. Una vez accedas a tu cuenta, no olvides cambiarla en tu perfil por una más fácil de recordar.
-              </h4>
-              <hr>
-              <p>Si no has sido tú, por favor, ignora este email.</p>
-          </body>
+            <body style="font-family: Arial, sans-serif; margin: 20px; color: #333;">
+              <h3>¡Hola!</h3>
+                  <p>¿Olvidaste tu contraseña? ¡No hay problema! Hemos recibido una solicitud para recuperar la contraseña de tu cuenta en Time Organizer.</p>
+                    <hr style="border: 1px solid #ddd; margin: 20px 0;">
+                  <p>Para continuar, usa el siguiente código en la página a la que te hemos redirigido:</p>
+                <h4><strong>${recoverPassCode}</strong></h4>
+                  <p>Con este código, tu correo electrónico y una nueva contraseña podrás recuperar tu cuenta. Así que, ¡no esperes más y haz clic en "RECUPÉRALA"!</p>
+                  <p>Si no fuiste tú quien solicitó este cambio, simplemente ignora este correo. Si necesitas ayuda, no dudes en contactarnos. ¡Estamos aquí para ayudarte!</p>
+                    <hr style="border: 1px solid #ddd; margin: 20px 0;">
+                  <p>Un abrazo virtual,</p>
+                  <p><strong>El equipo de Time Organizer</strong></p>
+            </body>
           </html>
-    `;
+`;
 
     await sendMail(email, emailSubject, emailBody);
 
